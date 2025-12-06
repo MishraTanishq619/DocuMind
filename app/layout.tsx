6d658cd89acd60b2670adc9d2433582110a1c0a7
MishraTanishq619 <mishratanishq619@gmail.com>
2025-12-06 23:28:01 +0530

    icon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/documind_icon.png" type="image/png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
